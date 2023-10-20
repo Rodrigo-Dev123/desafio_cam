82ce3636f76455e5ad3cc3309c721f271d8e6d45
Rodrigo <rodrigojsg9@gmail.com>
2023-10-20 09:51:27 -0300

    added pagination component and functionality to delete photo from localStorage

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import React from "react";
+
+const Pagination = ({ pages, currentPage, setCurrentPage }) => {
+    const qtdButtuns = 10;
+    const [start, setStart] = useState(qtdButtuns);
+    const [end, setEnd] = useState(0);
+
+    const setNext = () => {
+        setStart(start + qtdButtuns)
+        setEnd(end + qtdButtuns)
+    };
+    
+    const setBack = () => {
+        setEnd(end - qtdButtuns)
+        setStart(start - qtdButtuns)
+    };
+
+    console.log(end, start)
+    console.log(end === 0)
+    console.log(pages)
+
+    return (
+        <>
+            <div>{Array.from(Array(pages), (item, index) => {
+                return <button
+                    key={index}
+                    style={index === currentPage ? { backgroundColor: "gray" } : null}
+                    value={index}
+                    onClick={(e) => setCurrentPage(Number(e.target.value))}
+                >
+                    {index + 1}
+                </button>
+            }).slice(end, start)}</div>
+            
+            {end !== 0 && <button onClick={setBack}>back</button>}
+            {start < pages && <button onClick={setNext}>next</button>}
+            
+        </>
+    )
+};
+
+export default Pagination;
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,38 +1,70 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Pagination from '../../components/Pagination';
+import './styles.css';
 
-function App() {
+function Home() {
   const [items, setItems] = useState([]);
+  const [itemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const pages = Math.ceil(items.length / itemsPerPage);
+  const currentItems = items.slice(startIndex, endIndex);
+
+  const removeItem = (id) => {
+    const updatedItems = JSON.parse(localStorage.getItem('items')).filter(item => {
+      return item.id !== id; 
+    })
+      
+    localStorage.setItem('items', JSON.stringify(updatedItems))
+    setItems(JSON.parse(localStorage.getItem('items')));
+    console.log(updatedItems)
+    alert('Foto apagada com sucesso!');  
+  }
 
   useEffect(() => {
+    if (!localStorage.getItem('items')) {
+      console.log('Execultado!!!')
       const fetchData = async () => {
         const result = await fetch('https://jsonplaceholder.typicode.com/photos')
           .then(response => response.json())
           .then(data => data)
 
-          setItems(result);
+
+        localStorage.setItem('items', JSON.stringify(result))
       }
       fetchData();
+    }
+
+    setItems(JSON.parse(localStorage.getItem('items')));
 
   }, [])
 
   return (
     <section className='container'>
       <div className="App">
-        {items.map((photo) => {
+        {currentItems.map((photo) => {
           return (
             <div key={photo.id} className="post">
               <img src={photo.url} alt={photo.title}></img>
               <div className='post-content'>
                 <p>{photo.title}</p>
                 <h1>{photo.id}</h1>
+                <button onClick={() => removeItem(photo.id)}>Excluir</button>
               </div>
             </div>
           );
         })}
       </div>
+
+      <Pagination
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </section>
   );
 }
 
-export default App;
+export default Home;
